0d39b8c4b20cc334cdb8f5506ca12ffb22696b12
klappdev <koowaah@gmail.com>
2019-02-07 16:04:33 +0200

    Update res pictures

--- src/org/kl/Starter.java
+++ src/org/kl/Starter.java
@@ -4,7 +4,7 @@ import org.kl.contract.Ensures;
 import org.kl.contract.Expects;
 import org.kl.contract.Invariant;
 
-@Invariant("x != 0 && y >= -1")
+@Invariant("x != 0 && y != 0")
 public class Starter {
     private int x;
     private int y;
@@ -14,20 +14,32 @@ public class Starter {
         this.y = 10;
     }
 
-
     @Expects("x > 0")
     @Ensures("result < x")
-    public double sqrt(int x) {
+    private double sqrt(int x) {
         return Math.sqrt(x);
     }
 
-
-
-
     @Expects("x > -1 && x > y")
-    public boolean compare(float x, float y) { return x > y; }
+    private boolean compare(float x, float y) {
+        return x > y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 
     public static void main(String[] args) {
         Starter starter = new Starter();
+
+        int x = 20;
+        int y = 15;
+
+        System.out.println("sqrt: " + starter.sqrt(x));
+        System.out.println("compare : " + starter.compare(x, y));
     }
 }
